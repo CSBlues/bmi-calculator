004ca0f71362dd964e893456c29256a733b10c9e
CSBlues <66141547+CSBlues@users.noreply.github.com>
2020-10-05 08:53:01 -0400

    Initial commit

---
+++ src/main/java/com/example/project/Main.java
@@ -0,0 +1,7 @@
+package com.example.project;
+
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Not the right string, tests will fail!");
+    }
+}
